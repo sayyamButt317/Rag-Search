4d3bb52eef54b92fd86fd0c23a4eaf8e54346792
sayyamButt317 <sayyamButt317@gmail.com>
2025-08-01 22:45:15 +0500

    add route for video generation

--- Backend/src/Routes/ai.routes.js
+++ Backend/src/Routes/ai.routes.js
@@ -3,11 +3,15 @@ import CreateVectorEmbedding from "../controllers/ai.controller.js";
 import { TextToSpeech } from "../controllers/tts.controller.js";
 import {UploadPDF} from "../controllers/upload-file.controller.js";
 import { upload } from "../Config/multer.config.js";
+import VideoGenerator from "../controllers/VideoGenerator.controller.js";
 
 const router = express.Router();
 
+// AI Routes
 router.post("/upload/pdf", upload.single("pdf"), UploadPDF);
 router.get("/chat", CreateVectorEmbedding);
+router.post("/video-prompt", VideoGenerator);
 router.post("/text", TextToSpeech);
+
 
 export default router;
