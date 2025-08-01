26867a574f0f74e94f92a6d9ac659c179e949d63
sayyamButt317 <sayyamButt317@gmail.com>
2025-08-01 22:43:34 +0500

    add bull MQ in Upload function

--- Backend/src/controllers/upload-file.controller.js
+++ Backend/src/controllers/upload-file.controller.js
@@ -1,20 +1,11 @@
-import { Queue } from "bullmq";
-
-
-const queue = new Queue("file-upload-queue", {
-  connection: {
-    host: "localhost",
-    port: "6379",
-  },
-});
-
+import BullQueue from "../Config/BullQueue.js";
 export async function UploadPDF(req,res) {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
 
-    await queue.add("file-ready", {
+    await BullQueue.add("file-ready", {
       filename: req.file.originalname,
       destination: req.file.destination,
       path: req.file.path,
