29ba801f7825736f928942cdf10c20e73ee6f021
sayyamButt317 <sayyamButt317@gmail.com>
2025-08-25 11:29:12 +0500

    deleted files

--- Backend/src/services/ai.service.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from 'openai';
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
-
-const client = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-})
-
-export default client;
--- Backend/src/services/embedding.service.js
+++
@@ -1,7 +0,0 @@
-import { OpenAIEmbeddings } from "@langchain/openai";
-
-export const embeddings = new OpenAIEmbeddings({
-  modelName: "text-embedding-3-small",
-  apiKey: process.env.OPENAI_API_KEY,
-});
-export default embeddings;
